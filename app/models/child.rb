bb5217b6dd1d9228c8ce04c7a3858e26c44d1b22
Saturn226 <serenehopes@gmail.com>
2016-12-20 23:00:54 -0500

    Added some backend validations

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,6 +2,8 @@ class Child < ActiveRecord::Base
   belongs_to :klass
   has_many :events, dependent: :destroy
   accepts_nested_attributes_for :events
+  validates :name, presence: true
+  validates :age, presence: true
 
   def name
     self.first_name + " " + self.last_name
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,7 @@ class Event < ActiveRecord::Base
   belongs_to :activity
   belongs_to :child
   accepts_nested_attributes_for :activity
+
+  validates :activity_id, presence: true
 end
 
